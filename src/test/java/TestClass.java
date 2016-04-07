0d860e20df26f0d77fd69e783f6043b3392bc286
Revill1 <adamredlinski@gmail.com>
2016-04-07 15:14:19 +0200

    Dodany test dla metody add klasy Money

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -12,10 +12,21 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class TestClass {
 
 	@Test
-	public void MultiMoneyTest() 
+	public void multiMoneyTest() 
+	 		{
+	 			Money money = new Money(4);
+	 			
+	 			Assert.assertEquals(true, money.multiplyBy(5).equals(new Money(20)));
+	 		}
+	
+	@Test
+	public void addMoneyTest() 
 	 		{
 	 			Money money = new Money(4);
 	 			Money other = new Money(5);
-	 			Assert.assertEquals(false, money.multiplyBy(2).equals(new Money(20)));
+	 			Money other2 = new Money(10);
+	 			Money other3 = new Money (20);
+	 			
+	 			Assert.assertEquals(true, money.add(other).add(other3).add(other2).equals(new Money(39)));
 	 		}
 }
